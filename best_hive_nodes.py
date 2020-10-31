e6dc6be1dc9d74228d6168ca2b3a8886ceef57b3
Marky <33408375+officiallymarky@users.noreply.github.com>
2020-10-31 00:42:58 -0400

    Update default nodes & return all best nodes when scores too low

--- best_hive_nodes.py
+++ best_hive_nodes.py
@@ -2,24 +2,36 @@ import requests
 from tenacity import retry, stop_after_attempt, wait_exponential
 
 BECON_URL = 'https://beacon.peakd.com/api/best'
-DEFAULT_NODES = ['https://api.hive.blog', 'https://api.openhive.network', 'https://anyx.io']
+DEFAULT_NODES = ['https://api.hive.blog',
+                 'https://api.openhive.network',
+                 'https://anyx.io',
+                 'api.deathwing.me']
 
 
 def return_default_nodes(message):
-	print(message)
-	return DEFAULT_NODES
+    print(message)
+    return DEFAULT_NODES
+
 
 @retry(stop=stop_after_attempt(5),
-	   wait=wait_exponential(multiplier=1, min=0.5, max=5),
-	   retry_error_callback=return_default_nodes)
+       wait=wait_exponential(multiplier=1, min=0.5, max=5),
+       retry_error_callback=return_default_nodes)
 def get_best_hive_nodes(score=90):
-	node_list = requests.get(BECON_URL)
-	return find_best_nodes(node_list.json(), score)
+    node_list = requests.get(BECON_URL)
+    return find_best_nodes(node_list.json(), score)
+
 
 def find_best_nodes(node_list, score):
-	nodes = []
+    nodes = []
+    all_nodes = []
 
-	for node in node_list:
-		if node['score'] > score:
-			nodes.append(node['endpoint'])
-	return nodes
+    for node in node_list:
+        all_nodes.append(node['endpoint'])
+
+        if node['score'] > score:
+            nodes.append(node['endpoint'])
+
+    if nodes:
+        return nodes
+    else:
+        return all_nodes
